f29359d5eed0adac8923b45c8802642b07fdb1c6
Ben Haggard <21bhaggard@gmail.com>
2025-01-15 00:12:14 -0700

    mission 2 initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Dice
+{
+    private int value;
+    public int Roll()
+    {
+        Random random = new Random();
+        value = random.Next(1, 7);
+        return value;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        try
+        {
+            int rolls = int.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        // instantiate 2 new dice (could just use one and roll twice tho)
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        // array of length 11 for all dice combinations
+        int[] results = new int[11];
+        // fill all slots with starting value 0
+        for (int i = 0; i < results.Length; i++)
+        {
+            results[i] = 0;
+        }
+        for (int i = 0; i < rolls; i++)
+        {
+            int value = dice1.Roll() + dice2.Roll()
+            results[value - 2]++;
+        }
+    }
+}
